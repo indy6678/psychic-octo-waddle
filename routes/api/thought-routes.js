780f32dd6b27a688ca84815a31f7a916f4ca3419
Ed <105404937+indy6678@users.noreply.github.com>
2022-09-25 17:01:08 -0500

    updated thought-routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const {getAllThoughts, getThoughtById, createThought, updateThought, deleteThought} = require('../../controllers/thought-controller');
 
+// set up GET all and POST routes at /api/thoughts/
+router.route('/').get(getAllThoughts).post(createThought);
 
+// set up GET one, PUT, and DELETE routes at /api/thoughts/:id
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
 
 module.exports = router;
